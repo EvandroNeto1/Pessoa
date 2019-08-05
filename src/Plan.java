7037eb751edb760981240d3bac313ccc0a783b92
unknown <evandro_bruno-neto@saojose.sc.senai.br>
2019-08-05 12:29:20 -0300

    Criado Pessoa porem houve erro no final

---
+++ src/Plan.java
@@ -0,0 +1,12 @@
+
+public class Plan {
+	
+	public static void main(String[] args){
+		
+		Person Person1 = new Person(Reginaldo, 43, 1,80);
+		
+		Person1.setIdade(43);
+		
+		System.out.println("The person" + person1.getName + "has been sucesfully registred");
+		}
+}
